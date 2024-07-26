a534366ff7708eaf5332ad40b58bc8b15ad0aee0
Pistoy <emrecekic73@gmail.com>
2024-07-26 18:53:30 +0300

    Initial Commit

---
+++ C/CApp.swift
@@ -0,0 +1,17 @@
+//
+//  CApp.swift
+//  C
+//
+//  Created by Emre Cekic on 26.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
